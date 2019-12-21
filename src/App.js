5c829c4b5ebeec8993b81e6225224d64a9b698ba
Johnson <jo74705@gmail.com>
2019-12-21 19:43:51 +0800

    Init App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from "mobx-react";
 import { Scene, Router, Stack }  from 'react-native-router-flux'
+import {  Footer, FooterTab, Button, Text } from 'native-base';
 
 import * as stores from './store'
 
@@ -21,6 +22,22 @@ class App extends Component {
             <Scene key="home" component={Home} initial/>
           </Stack>
         </Router>
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Text>Apps</Text>
+            </Button>
+            <Button>
+              <Text>Camera</Text>
+            </Button>
+            <Button active>
+              <Text>Navigate</Text>
+            </Button>
+            <Button>
+              <Text>Contact</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Provider>
     )
   }
